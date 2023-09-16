3c0b92c026e50f92888c2a676450419f59682016
MichaelKattwinkel <mdkattwinkel@vt.edu>
2023-09-16 00:54:59 -0400

    Create initial API interface for flask backend

---
+++ backend/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+import ai_service
+from game_state import GameState
+
+app = Flask(__name__)
+
+
+@app.route('/')  # TODO: Emphasize this endpoint. It's spitting
+def who_is_different():
+    return 'Eugene is different'
+
+
+@app.route('/hello')  # TODO: Remove
+def hello_world():
+    return 'hello_world'
+
+
+@app.route('/firstmove')  # TODO: Remove
+# first move will always be the same, but will remove this
+def get_first_move():
+    return {
+        'column': 0,
+        'row': 3
+    }
+
+
+# SEE readme for example http post to hit this
+@app.route('/getnextmove', methods=['POST'])
+# receive in the game state, send AI's next move
+def get_next_move():
+    data = request.get_json()  # body of POST
+    board = data['board']
+    game_state = GameState(custom_board=board)
+
+    return ai_service.get_next_move(game_state)
+
+
+if __name__ == '__main__':
+    # Run the application on a local development server
+    app.run(debug=True)
