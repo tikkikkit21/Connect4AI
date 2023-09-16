529c5e8a5d5e2b35e0aa9e0327367c000c0e53c8
MichaelKattwinkel <mdkattwinkel@vt.edu>
2023-09-16 11:23:06 -0400

    Use numpy for array

--- backend/game_state.py
+++ backend/game_state.py
@@ -1,11 +1,16 @@
+import numpy as np
+
+ROWS = 6
+COLS = 7
+
 class GameState:
     def __init__(self, custom_board=None):
         if custom_board:
             # Use the custom board if provided
-            self.board = custom_board
+            self.board = np.array(custom_board)
         else:
             # Initialize a 7x6 board with empty slots (0 represents an empty slot)
-            self.board = [[0] * 7 for _ in range(6)]
+            self.board = np.zeros((ROWS, COLS))
 
     def __str__(self):
         # Create a string representation of the board
@@ -16,9 +21,9 @@ class GameState:
 
     def make_move(self, column, player):
         # Check if the column is valid and the selected slot is empty
-        if 0 <= column < 7 and self.board[0][column] == 0:
+        if 0 <= column < COLS and self.board[0][column] == 0:
             # Find the lowest empty slot in the column and place the player's piece (1 or 2)
-            for row in range(5, -1, -1):
+            for row in range(ROWS-1, -1, -1):
                 if self.board[row][column] == 0:
                     self.board[row][column] = player
                     return True  # Move successfully made
@@ -33,19 +38,19 @@ class GameState:
 
     def check_winner(self, player):
         # Check for a winning condition in rows, columns, and diagonals
-        for row in range(6):
-            for col in range(7):
+        for row in range(ROWS):
+            for col in range(COLS):
                 if self.board[row][col] == player:
                     # Check horizontal
-                    if col + 3 < 7 and all(self.board[row][col + i] == player for i in range(4)):
+                    if col + 3 < COLS and all(self.board[row][col + i] == player for i in range(4)):
                         return True
                     # Check vertical
-                    if row + 3 < 6 and all(self.board[row + i][col] == player for i in range(4)):
+                    if row + 3 < ROWS and all(self.board[row + i][col] == player for i in range(4)):
                         return True
                     # Check diagonal (up-right)
-                    if col + 3 < 7 and row - 3 >= 0 and all(self.board[row - i][col + i] == player for i in range(4)):
+                    if col + 3 < COLS and row - 3 >= 0 and all(self.board[row - i][col + i] == player for i in range(4)):
                         return True
                     # Check diagonal (down-right)
-                    if col + 3 < 7 and row + 3 < 6 and all(self.board[row + i][col + i] == player for i in range(4)):
+                    if col + 3 < COLS and row + 3 < ROWS and all(self.board[row + i][col + i] == player for i in range(4)):
                         return True
         return False  # No winner
