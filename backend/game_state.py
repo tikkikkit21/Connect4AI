0a910dc45962af0b6eb0749241c2bc19fdaf0c7e
MichaelKattwinkel <mdkattwinkel@vt.edu>
2023-09-16 15:35:09 -0400

    run formatter

--- backend/game_state.py
+++ backend/game_state.py
@@ -6,6 +6,7 @@ COLS = 7
 PLAYER_PIECE = 1
 AI_PIECE = 2
 
+
 class GameState:
     def __init__(self, custom_board=None):
         if custom_board:
@@ -35,15 +36,15 @@ class GameState:
 
     def is_full(self):
         # Check if the board is completely filled (a tie)
-        for top_of_col in self.board[:,ROWS-1]:
+        for top_of_col in self.board[:, ROWS-1]:
             if top_of_col == 0:
                 return False  # There's an empty slot
         return True  # The board is full (tie game)
-    
+
     def check_win_condition(self, piece: int):
         """
         checks win condition for player
-        
+
         Args:
         piece: This should be 1 for the players peice, or 2 for the AI's.
         """
@@ -66,14 +67,14 @@ class GameState:
                     return True
 
         # check downward diagonals
-        for c in range(3,COLS):
+        for c in range(3, COLS):
             for r in range(3, ROWS):
                 if self.board[c][r] == piece and self.board[c-1][r-1] == piece and self.board[c-2][r-2] == piece and self.board[c-3][r-3] == piece:
                     return True
-        
+
         # no win
         return False
-    
+
     def get_landing_row(self, col: int):
         """
         Given a column, determines where the peice lands, or -1 if full.
@@ -86,4 +87,3 @@ class GameState:
             if column[r] == 0:
                 return r
         return -1
-
