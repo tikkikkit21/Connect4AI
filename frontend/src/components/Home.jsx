8259a66f0fcf377d6f5733fef48d19a6569792ba
eugene0628 <youjeanz@gmail.com>
2023-09-17 00:58:02 -0400

    Added menu CSS styling

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,11 +2,37 @@ import {NavLink} from "react-router-dom";
 
 function Home() {
     return (
-        <main> 
+        <main className="main-menu"> 
             <h1> Connect4 AI </h1>
-            <NavLink to={"/player-v-player"}>Player vs. Player</NavLink>
-            <NavLink to={"/player-v-bot"}>Player vs. Bot</NavLink>
-            <NavLink to={"/analyzer"}>Game Analyzer</NavLink>
+            <div className="menu-button-container">
+                <NavLink style = {{ textDecoration: "none"}} to={"/player-v-player"}>
+                    <div className="circle-class outer-button one">
+                        <div className="circle-class button-container">
+                            <div>
+                                <h3>Player vs. Player</h3>
+                            </div>
+                        </div>
+                    </div>
+                </NavLink>
+                <NavLink style = {{ textDecoration: "none"}} to={"/player-v-bot"}>
+                <div className="circle-class outer-button two">
+                        <div className="circle-class button-container">
+                            <div>
+                                <h3>Player vs. Bot</h3>
+                            </div>
+                        </div>
+                    </div>
+                </NavLink>
+                <NavLink style = {{ textDecoration: "none"}} to={"/analyzer"}>
+                <div className="circle-class outer-button one">
+                        <div className="circle-class button-container">
+                            <div>
+                                <h3>Game Analyzer</h3>
+                            </div>
+                        </div>
+                    </div>
+                </NavLink>
+            </div>
         </main>
     )
 }
