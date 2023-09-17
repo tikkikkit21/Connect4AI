253c572518a7ba2ef9ffed86269ba9831a710f1a
Louis Zhang <louiszhang@Louiss-MacBook-Air.local>
2023-09-17 02:46:27 -0400

    BotPage.jsx merged

--- frontend/src/components/BotPage.jsx
+++ frontend/src/components/BotPage.jsx
@@ -1,12 +1,13 @@
 import Header from './Header.jsx'
+import Board from './Board.jsx'
 
 function BotPage() {
     return (
         <div>
             <Header />
             <p>This is the bot page.</p>
+            <Board gameMode={"pve"} onTurn={() => {}}/>
         </div>
-        
     )
 }
 
