42b3c841f6522951a09ae8a22fd1c0254402190f
louiszhangg <144161490+louiszhangg@users.noreply.github.com>
2023-09-17 03:21:28 -0400

    finished (#21)
    Co-authored-by: Louis Zhang <louiszhang@Louiss-MacBook-Air.local>

--- frontend/src/components/BotPage.jsx
+++ frontend/src/components/BotPage.jsx
@@ -1,12 +1,23 @@
-import Header from './Header.jsx'
-import Board from './Board.jsx'
+import Header from './Header.jsx';
+import { useState } from 'react';
+import Board from './Board.jsx';
 
 function BotPage() {
+    const [string, setString] = useState("It is the Player's Turn")
+    const handleTurnChange = (turn) => {
+        if (turn % 2 === 0) {
+            setString("It is the Player's Turn");
+        }
+        else {
+            setString("The AI is Taking its Turn...");
+        }
+    }
     return (
         <div>
             <Header />
-            <p>This is the bot page.</p>
-            <Board gameMode={"pve"} onTurn={() => {}}/>
+            <h1>This is the bot page.</h1>
+            <h2>{string}</h2>
+            <Board gameMode={"pve"} onTurn={handleTurnChange}/>
         </div>
     )
 }
