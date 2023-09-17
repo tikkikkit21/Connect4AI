ad7ae9b651f627f51fba9b2002beefdcf0f615c3
MichaelKattwinkel <72370966+MichaelKattwinkel@users.noreply.github.com>
2023-09-17 00:06:31 -0400

    fix conflicts (#16)
    * Initialize frontend
    
    * added main page functionality
    
    * added @mui/material jaunts
    
    * Added a circle
    
    * got them d3.js
    
    * grid with minimal functionality
    
    * clicking on the jaunts kinda works
    
    * backend board works perfectly
    
    * THE GRID FINALLY WORKS NOW (pvp not pvAI)
    
    ---------
    
    Co-authored-by: tikkikkit21 <tikkikkit21@gmail.com>
    Co-authored-by: Louis Zhang <louiszhang@Louiss-MacBook-Air.local>

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,104 +1,112 @@
 import Circle from './Circle.jsx';
 import { useState, useEffect } from 'react';
+import * as React from 'react';
 
-function Board() {
+class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.getBoardFromBackend = this.getBoardFromBackend.bind(this);
+        this.updateBoard = this.updateBoard.bind(this);
+        this.generateStartingGrid = this.generateStartingGrid.bind(this);
+        this.state = {
+            board: this.getBoardFromBackend(),
+            grid: this.generateStartingGrid(),
+            turn: 0, // 0 when player 1 is playing, 1 when player 2 is playing
+        }
+    }
+
     // Outer array: 7 columns; inner array: 6 rows. First number: lowest position; last number: highest position
     // Hard-coded for now
-    const getBoardFromBackend = () => {
-        const BACKEND_ADDR = "http://127.0.0.1:5000";
-        const url = BACKEND_ADDR + "/getnextboard"
-        
-        // TODO: need to get current board instead of blank
-        const currBoard = 
-            [[0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0]]
-        const data = {'board': currBoard}
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        };
-        let new_board = []
-        console.log("Sent request to backend for move and new board")
-        fetch(url, requestOptions)
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            return response.json();
-          })
-          .then((data) => {
-            new_board = data
-            console.log("Recieved move and new board from backend: ", new_board)
-          })
-          .catch((error) => {
-            console.error('There was a problem with the fetch operation:', error);
-          });
-        return new_board;
+    getBoardFromBackend() {
+        return [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
     }
 
-    const [board, setBoard] = useState(getBoardFromBackend())
-    // Actual turns start at 1
-    const [turn, setTurn] = useState(0)
-    
-    useEffect(() => {
-        console.log(turn);
-        // You can perform actions that depend on the updated 'turn' here.
-      }, [turn]);
-
-    const updateBoard = (column) => {
-        console.log("updating board")
-        let array = board;
-        let j = 0;
-        while (array[column][j] != 0) {
-            j++;
-        }
-        array[column][j] = 1
-        setBoard(array);
-        setTurn(turn => turn + 1)
-        updateGrid(column, j, turn);
+    updateBoard(column) {
+        let i = -1;
+        let updated = false;
+        const nextBoard = this.state.board.map((currCol) => {
+            i++;
+            return currCol.map((cell) => {
+                if (i === column && cell == 0 && !updated) {
+                    updated = true;
+                    return this.state.turn + 1;
+                }
+                else {
+                    return cell;
+                }
+            })
+        })
+        this.setState(prevState => ({
+            board: nextBoard,
+            turn: (prevState.turn + 1) % 2
+        }))
     }
 
-    const generateStartingGrid = () => {
+    generateStartingGrid() {
         let code = []
         for (let i = 5; i > -1; i--) {
             for (let j = 0; j < 7; j++) {
-                code.push(<Circle value='zero' column={j} keyProps={`${j}${i}`} handleClick={updateBoard} key={`${j}${i}`}/>)
+                code.push(<Circle value='zero' column={j} keyProps={`${j}${i}`} handleClick={this.updateBoard} key={`${j}${i}`}/>)
             }
         }
         return code
     }
 
-    const [grid, setGrid] = useState(generateStartingGrid())
-
-    const updateGrid = (column, row, turn) => {
-        let array = grid;
-        let i = 0;
-        let key = `${column}${row}`;
-        while (!(array[i].props.keyProps === key)) {
-            i++;
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.board != prevState.board && this.state.turn != prevState.turn) {
+            let actionColumn = 0;
+            let actionRow = 0;
+            // Find the column and the row of the updated value by finding the different index between previous board and current board
+            for (let i = 0; i < 7; i++) {
+                for (let j = 0; j < 6; j++) {
+                    if (this.state.board[i][j] !== prevState.board[i][j]) {
+                        actionColumn = i;
+                        actionRow = j;
+                    }
+                }
+            }
+            let array = this.state.grid;
+            let i = 0;
+            let key = `${actionColumn}${actionRow}`;
+            while (!(array[i].props.keyProps === key)) {
+                i++;
+            }
+            if (this.state.turn % 2 === 0) {
+                array[i] = <Circle value="one" column={actionColumn} keyProps={`${actionColumn}${actionRow}`} handleClick={this.updateBoard} key={`${actionColumn}${actionRow}`}/>
+            }
+            else {
+                array[i] = <Circle value="two" column={actionColumn} keyProps={`${actionColumn}${actionRow}`} handleClick={this.updateBoard} key={`${actionColumn}${actionRow}`}/>
+            }
+            this.setState(prevState => ({
+                grid: array
+            }))
         }
-        if (turn % 2 === 0) {
-            array[i] = <Circle value="one" column={column} keyProps={`${column}${row}`} handleClick={updateBoard} key={`${column}${row}`}/>
-        }
-        else {
-            array[i] = <Circle value="two" column={column} keyProps={`${column}${row}`} handleClick={updateBoard} key={`${column}${row}`}/>
-        }
-        setGrid(array);
+        
     }
 
-    return (
-        <div className="game-grid">
-            {grid}
-        </div>
-    )
+    // const updateGrid = (column, row, turn) => {
+    //     let array = grid;
+    //     let i = 0;
+    //     let key = `${column}${row}`;
+    //     while (!(array[i].props.keyProps === key)) {
+    //         i++;
+    //     }
+    //     if (turn % 2 === 0) {
+    //         array[i] = <Circle value="one" column={column} keyProps={`${column}${row}`} handleClick={updateBoard} key={`${column}${row}`}/>
+    //     }
+    //     else {
+    //         array[i] = <Circle value="two" column={column} keyProps={`${column}${row}`} handleClick={updateBoard} key={`${column}${row}`}/>
+    //     }
+    //     setGrid(array);
+    // }
+
+    render() {
+        return (
+            <div className="game-grid">
+                {this.state.grid}
+            </div>
+        )
+    }
 }
 
 export default Board;
